aa30c02ca614d20a242bc42fbc2a8d781978f307
Lappas123 <lappas>
2018-04-12 14:40:05 +0200

    irfunction

--- Car4Park.ino
+++ Car4Park.ino
@@ -1,3 +1,5 @@
+
+
 #include <Smartcar.h>
 
 Gyroscope gyro;
@@ -5,6 +7,7 @@ Odometer encoderLeft, encoderRight;
 Car car;
 SR04 front;
 GP2Y0A21 sideFrontIR; //measure distances between 12 and 78 centimeters
+SR04 back;
 
 const int SIDE_FRONT_PIN = A8;
 const float fSpeed = 0.5; //a ground speed (m/sec) for going forward
@@ -15,90 +18,89 @@ const int encoderLeftPin = 2;
 const int encoderRightPin = 3;
 const int TRIGGER_PIN = 6; //D6
 const int ECHO_PIN = 7; //D7
+const int ECHO_PINb = A13;
+const int TRIGGER_PINb = A12;
+int count = 0;
 
 void setup() {
+  
+  back.attach(TRIGGER_PINb,ECHO_PINb);
   gyro.attach();
   sideFrontIR.attach(SIDE_FRONT_PIN);
   front.attach(TRIGGER_PIN, ECHO_PIN); //trigger pin, echo pin
   encoderLeft.attach(encoderLeftPin);
   encoderRight.attach(encoderRightPin);
+  //
   delay(1500);
-  Serial3.begin(9600);
+  //Serial3.begin(9600);
   Serial.begin(9600);
   encoderLeft.begin();
   encoderRight.begin();
   gyro.begin(90);
-  int offset = gyro.calibrate();
-  Gyroscope gyro(offset);
+  Gyroscope gyro();
   car.begin(encoderLeft, encoderRight); //initialize the car using the encoders
-  car.enableCruiseControl(); //using default PID values
+  // car.enableCruiseControl(); //using default PID values
  //car.enableCruiseControl(12,5,17,30); //using custom values and control frequency
-  car.setSpeed(fSpeed);
+ car.setMotorSpeed(30,30);
+ delay(100);
+ car.setMotorSpeed(66,100);
 }
 
 void loop() {
-  gyro.update();
+  
+ //  int offset = gyro.calibrate();
+ // gyro.update(offset);
   car.updateMotors();
-  straight();
-  handleInput();
-  distance();
+  ir();
+  // straight();
+ // distance();
+Serial.println(sideFrontIR.getDistance());
+ 
+
+  }
+ 
+void ir(){
+ 
+  while(sideFrontIR.getDistance() > 20){
+    delay(5); 
+  count++;
+    if(count > 100){
+    car.setSpeed(0);
+    }
+    
+  }
+  
 }
 
-void straight (){
-  
-  if (gyro.getAngularDisplacement() > 1 && gyro.getAngularDisplacement() < 180){
-   // car.setSpeed(fSpeed);
+
+
+
+
+/*void straight (){
+
+  Serial.println(gyro.getAngularDisplacement());
+  if (gyro.getAngularDisplacement() > 0 && gyro.getAngularDisplacement() < 180){
     car.setAngle(-30);
   }
 
-  if (gyro.getAngularDisplacement() < 350 && gyro.getAngularDisplacement() > 180){
-   // car.setSpeed(fSpeed);
-    car.setAngle(5);
+  if (gyro.getAngularDisplacement() < 360 && gyro.getAngularDisplacement() > 180){
+    car.setAngle(30);
   }
 }
-
+*/
 void distance (){
-  Serial.println(front.getDistance());
+ // Serial.println(front.getDistance());
  // Serial.println(sideFrontIR.getDistance());
-  if(sideFrontIR.getDistance() < 20 && sideFrontIR.getDistance() > 0 ){
+  if(front.getDistance() < 20 && front.getDistance() > 0 ){
     car.setSpeed(bSpeed);
-    car.setAngle(lDegrees);
     delay(1000);
-    
-   // car.setSpeed(0);
+    car.setSpeed(0);
   }
 
-  if(sideFrontIR.getDistance() > 20){
+  if(front.getDistance() > 20){
     car.setSpeed(fSpeed);
   }
   delay (500);
 
 }
 
-void handleInput() { //handle serial input if there is any
- 
-  if (Serial3.available()) {
-    char input = Serial3.read(); //read everything that has been received so far and log down the last entry
-    switch (input) {
-      case 'l': //rotate counter-clockwise going forward
-        car.setSpeed(fSpeed);
-        car.setAngle(lDegrees);
-        break;
-      case 'r': //turn clock-wise
-        car.setSpeed(fSpeed);
-        car.setAngle(rDegrees);
-        break;
-      case 'f': //go ahead
-        car.setSpeed(fSpeed);
-        car.setAngle(0);
-        break;
-      case 'b': //go back
-        car.setSpeed(bSpeed);
-        car.setAngle(0);
-        break;
-      default: //if you receive something that you don't know, just stop
-        car.setSpeed(0);
-        car.setAngle(0);
-    }
-  }
-}
