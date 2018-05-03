5eca3f5619cce38bc2f4792cc65651948755e514
gusmalmgal <gusmalmgal@student.gu.se>
2018-05-03 16:23:08 +0200

    new finding parking and parking algorithm

--- Car4Park.ino
+++ Car4Park.ino
@@ -4,7 +4,7 @@ Odometer encoderLeft, encoderRight;
 Car car;
 SR04 front;
 SR04 back;
-
+SR04 side;
 GP2Y0A21 sideFrontIR; //measure distances between 12 and 78 centimeters
 const int SIDE_FRONT_PIN = A8;
 const float fSpeed = 0.5; //a ground speed (m/sec) for going forward
@@ -16,8 +16,13 @@ const int encoderRightPin = 3;
 const int TRIGGER_PIN = 6; //D6
 const int ECHO_PIN = 7; //D7
 const int TRIGGER_PIN1 = A12;
+const int TRIGGER_PIN2 = A8;
+const int ECHO_PIN2 = A9;
 const int ECHO_PIN1 = A13;
 int count = 0;
+int motorSpeed = 45; //40% of the max speed
+
+boolean backing = false;
 
 
 void setup() {
@@ -25,27 +30,32 @@ void setup() {
   sideFrontIR.attach(SIDE_FRONT_PIN);
   front.attach(TRIGGER_PIN, ECHO_PIN); //trigger pin, echo pin
   back.attach(TRIGGER_PIN1, ECHO_PIN1);
+  side.attach(TRIGGER_PIN2, ECHO_PIN2);
   encoderLeft.attach(encoderLeftPin);
   encoderRight.attach(encoderRightPin);
   delay(1500);
   Serial.begin(9600);
   Serial1.begin(9600);
   Serial3.begin(9600);
+ 
   gyro.begin(90);
   Gyroscope gyro();
   car.begin(encoderLeft, encoderRight); //initialize the car using the encoders
 
-car.setMotorSpeed(66,66);
-delay(30);
- car.setMotorSpeed(33,55);
-  
-  
+
+ car.setMotorSpeed(30, 45);
+   delay(10);
+    car.setMotorSpeed(20,31);
+
+
+  //rotateOnSpot(-45);
 }
 
 void loop(){
   
-Serial.println(back.getDistance());
+Serial.println(side.getDistance());
 car.updateMotors();
+
 ir();
 
 
@@ -60,20 +70,20 @@ void ir(){
 
   if(check == false){
     
-  if(front.getDistance()>20){
+  if(side.getDistance()>20){
   encoderLeft.begin();
   encoderRight.begin();
   check = true;
  }
  }
   
-  if(check == true && front.getDistance()>20){
+  if(check == true && side.getDistance()>20){
     
-    if(encoderLeft.getDistance()>60 && encoderRight.getDistance()>60){
+    if(encoderLeft.getDistance()>45 && encoderRight.getDistance()>45){
       car.setMotorSpeed(0,0);
       parking();
    
-  }else if(front.getDistance()<20 && encoderLeft.getDistance()<60 && encoderRight.getDistance()<60 ){
+  }else if(side.getDistance()<20 && encoderLeft.getDistance()<45 && encoderRight.getDistance()<45 ){
     check = false;
   }
   
@@ -88,13 +98,9 @@ void distance(){
  // Serial.println(front.getDistance();
  // Serial.println(sideFrontIR.getDistance());
  
-  if(back.getDistance() < 10 && back.getDistance() > 0 ){
-   /* car.setMotorSpeed(28,40);
-    delay(100);
-    car.setMotorSpeed(20,27);
-    delay(1000);*/
-    car.setSpeed(0);
-    
+  if(back.getDistance() < 15 && back.getDistance() > 0 ){
+
+    backing = true;
   
   }
 } 
@@ -105,14 +111,21 @@ void distance(){
 
 void parking(){
 
- while(sideFrontIR.getDistance()<14){
+if(side.getDistance()<20){
+ while(side.getDistance()<20){
   
-   car.setMotorSpeed(-32, -39);
+  
+ car.setMotorSpeed(-30, -45);
    delay(10);
     car.setMotorSpeed(-20,-31);
-  }  
-  car.setMotorSpeed(-20,-31);
-  delay(200);
+
+  }
+}else if(side.getDistance()>20 || side.getDistance()==0){
+   car.setMotorSpeed(-30, -45);
+   delay(10);
+    car.setMotorSpeed(-20,-31);
+    delay(300);
+}
 
 /*
  car.setMotorSpeed(-25,-33);
@@ -121,9 +134,12 @@ delay(1000);
 */
 
 
-car.setMotorSpeed(-50,50);
-delay(650); 
+rotateOnSpot(-50);
+delay(500);
 
+parkingFinal();
+
+rotateOnSpot(50);
 /*
 while(back.getDistance()>7 || back.getDistance()==0){
   car.setMotorSpeed(-20,-31);
@@ -132,12 +148,19 @@ while(back.getDistance()>7 || back.getDistance()==0){
 
 */
 
-car.setMotorSpeed(-22,-36);
-delay(1300);
+/*
+if(backing == false){
+car.setMotorSpeed(-20,-31);
+}
 
-car.setMotorSpeed(50,-50);
-delay(550);
 
+
+
+//delay(1300);
+
+rotateOnSpot(35);
+delay(500);
+*/
 /*
 while(back.getDistance()>7 || back.getDistance()==0){
   car.setMotorSpeed(-22,-36);
@@ -166,6 +189,41 @@ car.setMotorSpeed(0,0);
 
 }
 
+void parkingFinal(){
+  while(back.getDistance()>20 || back.getDistance()==0){
+  car.setMotorSpeed(-22,-36);
+
+}
+
+}
+
+
+void rotateOnSpot(int targetDegrees) {
+  targetDegrees %= 360; //put it on a (-360,360) scale
+  if (!targetDegrees) return; //if the target degrees is 0, don't bother doing anything
+  /* Let's set opposite speed on each side of the car, so it rotates on spot */
+  if (targetDegrees > 0) { //positive value means we should rotate clockwise
+    car.setMotorSpeed(motorSpeed, -motorSpeed); // left motors spin forward, right motors spin backward
+  } else { //rotate counter clockwise
+    car.setMotorSpeed(-motorSpeed, motorSpeed); // left motors spin backward, right motors spin forward
+  }
+  unsigned int initialHeading = gyro.getAngularDisplacement(); //the initial heading we'll use as offset to calculate the absolute displacement
+  int degreesTurnedSoFar = 0; //this variable will hold the absolute displacement from the beginning of the rotation
+  while (abs(degreesTurnedSoFar) < abs(targetDegrees)) { //while absolute displacement hasn't reached the (absolute) target, keep turning
+    gyro.update(); //update to integrate the latest heading sensor readings
+    int currentHeading = gyro.getAngularDisplacement(); //in the scale of 0 to 360
+    if ((targetDegrees < 0) && (currentHeading > initialHeading)) { //if we are turning left and the current heading is larger than the
+      //initial one (e.g. started at 10 degrees and now we are at 350), we need to substract 360, so to eventually get a signed
+      currentHeading -= 360; //displacement from the initial heading (-20)
+    } else if ((targetDegrees > 0) && (currentHeading < initialHeading)) { //if we are turning right and the heading is smaller than the
+      //initial one (e.g. started at 350 degrees and now we are at 20), so to get a signed displacement (+30)
+      currentHeading += 360;
+    }
+    degreesTurnedSoFar = initialHeading - currentHeading; //degrees turned so far is initial heading minus current (initial heading
+    //is at least 0 and at most 360. To handle the "edge" cases we substracted or added 360 to currentHeading)
+  }
+  car.stop(); //we have reached the target, so stop the car
+}
 
 
 
