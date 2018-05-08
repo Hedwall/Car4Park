298d6c04f2ee4705c12bce0464d7ef786c7fb73f
RoseBud13 <32515593+RoseBud13@users.noreply.github.com>
2018-05-08 15:38:27 +0200

    Update Java_jSSC_Uno.java

--- Car4Park/data/Java_jSSC_Uno.java
+++ Car4Park/data/Java_jSSC_Uno.java
@@ -1,5 +1,30 @@
 package data;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
 public class Java_jSSC_Uno {
 
+    public static void main(String[] args) {
+        SerialPort serialPort = new SerialPort("/dev/ttyACM0");
+        try {
+            System.out.println("Port opened: " + serialPort.openPort());
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Java_jSSC_Uno.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            System.out.println("Params setted: " + serialPort.setParams(9600, 8, 1, 0));
+            System.out.println("\"Hello World!!!\" successfully writen to port: " + serialPort.writeBytes("Java_jSSC_Uno".getBytes()));
+            System.out.println("Port closed: " + serialPort.closePort());
+        }
+        catch (SerialPortException ex){
+            System.out.println(ex);
+        }
+    }
+    
 }
